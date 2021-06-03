e670e89026077b4e8dead63dd6e155755e66f988
Edwin <el5091@g.rit.edu>
2021-06-03 14:13:00 -0400

    -made the main navigation bar "sticky" -added a new file for jquery (js)

---
+++ resources/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    /*
+    EXAMPLE: Turns the background color of h1 things, when clicked, to red
+    $('h1').click(function(){
+        $(this).css('background-color', '#ff0000');
+    })
+    */
+
+
+
+
+
+});
